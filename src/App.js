23ae5a9b7f275d77b9479773f574158fa4eae8b9
Robert Gravina <robert.gravina@gmail.com>
2017-02-11 15:45:58 +1100

    Organise imports.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import { Container, Content, View } from 'native-base';
 import {Scene, Router} from 'react-native-router-flux';
-import { connect, Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { connect, Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import Login from './components/Login';
 import routes from './redux/routes';
-
 const reducers = combineReducers({routes});
 const store = createStore(
   reducers,
   applyMiddleware(thunk)
 );
-
 const RouterWithRedux = connect()(Router);
 
 export default class App extends Component {
