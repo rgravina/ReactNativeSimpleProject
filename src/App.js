6f9299ea41eba3b20cca5078e626f2f9bd312cd5
Robert Gravina <robert.gravina@gmail.com>
2017-02-11 15:34:57 +1100

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import { Container, Content, View } from 'native-base';
 import {Scene, Router} from 'react-native-router-flux';
+import { connect, Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import Login from './components/Login';
+import routes from './redux/routes';
+
+const reducers = combineReducers({routes});
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+);
+
+const RouterWithRedux = connect()(Router);
 
 export default class App extends Component {
   render() {
     return (
-    <Router>
-      <Scene key="root">
-        <Scene key="login" component={Login} title="Login"/>
-      </Scene>
-    </Router>
+      <Provider store={store}>
+        <RouterWithRedux>
+          <Scene key="root">
+            <Scene key="login" component={Login} title="Login"/>
+          </Scene>
+        </RouterWithRedux>
+      </Provider>
     );
   }
 }
