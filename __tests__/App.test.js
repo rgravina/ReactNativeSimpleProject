1c4996b51bf617a70e01f26eb7790d47daba3e45
Robert Gravina <robert.gravina@gmail.com>
2017-02-20 21:42:43 +1100

    Add describe block for App

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -13,6 +13,8 @@ jest.mock('Linking', () => {
   }
 });
 
-it('renders main App component', () => {
-  const tree = renderer.create(<App />);
+describe('App', () => {
+  it('renders main App component', () => {
+    const tree = renderer.create(<App />);
+  });
 });
